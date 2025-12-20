ebab2898c226ac0760050d1ecc20de3a4e4a4032
neeti-yaav <neetiy32@gmail.com>
2025-12-20 22:53:28 +0530

    20Dec2025:22:53

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+  "https://api.currencyfreaks.com/latest";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
+
 
 // Populate dropdowns
 for (let select of dropdowns) {
